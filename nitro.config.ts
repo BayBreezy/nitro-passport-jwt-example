d0cb03b6fc8d11a9ff309f79230bfbd531a9f115
Behon Baker <53905713+BayBreezy@users.noreply.github.com>
2024-12-02 10:02:45 -0500

    chore: bump deps

--- nitro.config.ts
+++ nitro.config.ts
@@ -1,8 +1,11 @@
 //https://nitro.unjs.io/config
 export default defineNitroConfig({
   srcDir: "server",
+
   imports: {
     dirs: ["./server/services"],
   },
+
   serverAssets: [{ baseName: "templates", dir: "./templates" }],
+  compatibilityDate: "2024-12-02",
 });
