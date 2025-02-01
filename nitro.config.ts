3aec540d6ec4816a58d1088a4bf1920002ef89b0
Behon Baker <53905713+BayBreezy@users.noreply.github.com>
2025-02-01 09:48:04 -0500

    chore: update compose config

--- nitro.config.ts
+++ nitro.config.ts
@@ -1,11 +1,9 @@
 //https://nitro.unjs.io/config
 export default defineNitroConfig({
   srcDir: "server",
-
   imports: {
     dirs: ["./server/services"],
   },
-
   serverAssets: [{ baseName: "templates", dir: "./templates" }],
   compatibilityDate: "2024-12-02",
 });
